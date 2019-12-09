9efd7c116cbfc6a705c5ef8470cc656577982877
onyekachila <onyekachila@gmail.com>
2019-12-09 18:26:14 +0100

    Add attendee reponse

---
+++ EventDTO/AttendeeResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EventDTO
+{
+    public class AttendeeResponse : Attendee
+    {
+        public ICollection<Conference> Conferences { get; set; } = new List<Conference>();
+
+        public ICollection<Session> Sessions { get; set; } = new List<Session>();
+    }
+}
